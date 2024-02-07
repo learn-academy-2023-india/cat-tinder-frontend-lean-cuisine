e7035d4700caae4a47673de147ef3730f2ebc0b2
Sarah Proctor <sproctor950@gmail.com>
2024-02-06 20:47:05 -0800

    adds styling and layout improvements for home, not found, header, and footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,13 @@
 import React from "react"
-import toastKitty from "../assets/toast.jpeg"
+import toastKitty from "../assets/cat-home.png"
 
 const Home = () => {
   return (
-    <>
-      <h3>Find a Furry Friend</h3>
-      <img
-        src={toastKitty}
-        alt="Toast Kitty hard at work"
-        className="main-content-image"
-      />
-    </>
+    <img
+      src={toastKitty}
+      alt="Toast Kitty hard at work"
+      className="main-content-image"
+    />
   )
 }
 
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,16 +1,13 @@
 import React from "react"
-import notACat from "../assets/not-a-cat.jpeg"
+import notACat from "../assets/not-a-cat.png"
 
 const NotFound = () => {
   return (
-    <>
-      <img
-        src={notACat}
-        alt="cute little dog that is definitely not a cat"
-        className="main-content-image"
-      />
-      <h4>Oops, no cats here!</h4>
-    </>
+    <img
+      src={notACat}
+      alt="cute little dog that is definitely not a cat"
+      className="main-content-image"
+    />
   )
 }
 
